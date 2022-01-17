6b6d3cc8b7c2bdf96b6d6f1475f0385bc9dafa01
Allan Larini <allan.larini@gmail.com>
2022-01-17 15:17:10 -0300

    alteracao round

--- Geekuniversity/exercicio06.py
+++ Geekuniversity/exercicio06.py
@@ -1,4 +1,4 @@
 hora_trab = float(input("Informe a quantidade de horas trabalhadas! "))
 salario = float(input("Informe o salario/hora! "))
-total_mes = round((hora_trab*salario), 2)
-print("O salario desse mes eh de: R${0}".format(total_mes))
+total_mes = (hora_trab*salario)
+print("O salario desse mes eh de: R${0:.2f}".format(total_mes))
