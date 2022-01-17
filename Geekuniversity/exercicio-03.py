3b4342e13be6a3b89cf9c0f27d353ba9c6c5db5d
Allan Larini <allan.larini@gmail.com>
2022-01-17 13:42:16 -0300

    criacao

---
+++ Geekuniversity/exercicio-03.py
@@ -0,0 +1,4 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+soma = int(num1+num2)
+print("A soma de {0} e {1} eh de: {2}".format(num1, num2, soma))
