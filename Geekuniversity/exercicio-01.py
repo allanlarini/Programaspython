27e688ca69d72347949fec643241d6542409da61
Allan Larini <allan.larini@gmail.com>
2022-01-17 13:32:13 -0300

    criacao

---
+++ Geekuniversity/exercicio-01.py
@@ -0,0 +1,4 @@
+qtd_min = int(input("Digite a quantidade minima: "))
+qtd_max = int(input("Digite a quantidade maxima: "))
+estoque_medio = float(qtd_max+qtd_min)/2
+print("O estoque medio eh de: {0}".format(estoque_medio))
