fafaf3260166c42d937ad67eff095a1fd22b45b6
Allan Larini <allan.larini@gmail.com>
2022-01-18 11:18:07 -0300

    criacao

--- Geekuniversity/Secao-06/exercicio-01.py
+++ Geekuniversity/Secao-06/exercicio-01.py
@@ -1,5 +1,6 @@
-# entradas
 numero = int(input("Informe um numero: "))
-# processamento
 if numero > 100:
     print("O numero {0} eh maior que 100".format(numero))
+else:
+    numero = 0
+    print("O numero eh {0} por ser menor que 100".format(numero))
