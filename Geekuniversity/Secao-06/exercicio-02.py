4a5f918d0d127bce03e2cb3c2c57d18ff19f05dd
Allan Larini <allan.larini@gmail.com>
2022-01-18 11:29:29 -0300

    criacao

---
+++ Geekuniversity/Secao-06/exercicio-02.py
@@ -0,0 +1,9 @@
+valor = int(input("Digite um numero: "))
+if valor == 0:
+    print("O numero informado eh zero!")
+if valor > 0:
+    a = valor
+    print("O numero {0} eh positivo!".format(a))
+else:
+    b = valor
+    print("O numero {0} eh negativo!".format(b))
