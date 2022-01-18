6fbcb8495f506a1f48964b1e7f193cc46dd29909
Allan Larini <allan.larini@gmail.com>
2022-01-18 11:31:48 -0300

    adcionado elif para 0

--- Geekuniversity/Secao-06/exercicio-02.py
+++ Geekuniversity/Secao-06/exercicio-02.py
@@ -1,9 +1,9 @@
 valor = int(input("Digite um numero: "))
-if valor == 0:
-    print("O numero informado eh zero!")
 if valor > 0:
     a = valor
     print("O numero {0} eh positivo!".format(a))
+elif valor == 0:
+    print("O numero informado eh zero!")
 else:
     b = valor
     print("O numero {0} eh negativo!".format(b))
