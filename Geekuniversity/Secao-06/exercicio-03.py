b95399f5602c6edb27264dc76ca63fb34e15e70c
Allan Larini <allan.larini@gmail.com>
2022-01-18 12:00:19 -0300

    criacao

---
+++ Geekuniversity/Secao-06/exercicio-03.py
@@ -0,0 +1,7 @@
+valor = int(input("Informar um numero: "))
+if valor % 2 == 0:
+    print("O numero eh positivo!")
+elif valor == 0:
+    print(" O numero eh neutro!")
+else:
+    print("O numero eh negativo!")
